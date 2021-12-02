9187b5069a8f7a80d03979fb87fbb9248c8580e6
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2021-12-02 10:55:10 +0100

    move #!/bin/sh -l to top of sh file

--- .github/actions/databricks-unit-test/entrypoint.sh
+++ .github/actions/databricks-unit-test/entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -l
+
 # Copyright 2020 Energinet DataHub A/S
 #
 # Licensed under the Apache License, Version 2.0 (the "License2");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#!/bin/sh -l
 
 cd ./source/databricks/tests/
 #Build wheel
