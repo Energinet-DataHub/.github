14aca8fcff0dd97b11f4e5cec888b62b227992f6
Kristian Schneider <khs@thamesconsulting.com>
2022-04-02 07:47:06 +0200

    .

--- .github/actions/databricks-unit-test/entrypoint.sh
+++ .github/actions/databricks-unit-test/entrypoint.sh
@@ -21,7 +21,7 @@ cd source/databricks/tests/
 #Build wheel
 # python ../setup.py install
 # python coverage-threshold install
-pip install coverage-threshold delta-spark
+pip install coverage-threshold
 coverage run --branch -m pytest .
 # Create data for threshold evaluation
 coverage json
