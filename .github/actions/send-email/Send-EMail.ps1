23dcfdbd81b7dc329e30ac4accb94de641d9389e
Dan Stenrøjl <XDAST@energinet.dk>
2023-02-24 11:56:48 +0100

    Script parameters

--- .github/actions/send-email/Send-EMail.ps1
+++ .github/actions/send-email/Send-EMail.ps1
@@ -1,3 +1,17 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 <#
     .SYNOPSIS
     Send emails using SendGrid.
@@ -8,10 +22,30 @@
 #>
 
 param (
-    # Should contain either a single email or a list of comma separated emails.
+    # A valid SendGrid API key.
     [Parameter(Mandatory = $true)]
     [string]
-    $EMail
+    $SendGridApiKey,
+    # The name of the team who should receive the email. When sending to multiple recipients this name will be used for all of them.
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TeamName,
+    # The email to-address. Should contain either a single email or a list of comma separated emails.
+    [Parameter(Mandatory = $true)]
+    [string]
+    $To,
+    # The email from-address.
+    [Parameter(Mandatory = $true)]
+    [string]
+    $From,
+    # The email subject. Should contain environment information when possible.
+    [Parameter(Mandatory = $true)]
+    [string]
+    $Subject,
+    # Additional content for the email body. Apart from this the email body will also always contain a link to the failed build.
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Content
 )
 
-Write-Host "Email: '$EMail'"
+Write-Host "To: '$To'"
