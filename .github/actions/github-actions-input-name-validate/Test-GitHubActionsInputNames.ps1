d72613ee4fee53dd4d7184a24d120621c9a4e9a9
Dan Stenrøjl <XDAST@energinet.dk>
2023-03-15 16:13:37 +0100

    Move validation to script

---
+++ .github/actions/github-actions-input-name-validate/Test-GitHubActionsInputNames.ps1
@@ -0,0 +1,57 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+Import-Module PowerShell-Yaml -Force
+
+<#
+    .SYNOPSIS
+    Determines whether all input parameters in GitHub action and workflow files are valid.
+
+    .DESCRIPTION
+    Determines whether all input parameters in GitHub action and workflow files are lowercase.
+    It validates the definitions as well as any usage of all input patameters.
+    It returns `$True` if all input parameters are valid; otherwise `$False`.
+#>
+function Test-GitHubActionsInputNames {
+    param (
+        # The folder path in which to search for relevant files to validate.
+        [Parameter(Mandatory = $true)]
+        [string]
+        $FolderPath
+    )
+    $isValid = $true
+
+    $files = Get-ChildItem -Path $folder -Recurse -File -Include ('*.yml', '*.yaml')
+    Write-Host "Files found in $($folder): $($files.Length)"
+
+    foreach ($file in $files) {
+        Write-Host "Checking $($file.FullName)"
+
+        $yaml = Get-Content -Path $file.FullName | Out-String
+
+        $yaml = $yaml.Replace('{{', '').Replace('}}', '')
+        $jsonObj = (ConvertFrom-Yaml -Yaml $yaml)
+        $inputKeys = $jsonObj.on.workflow_call.inputs.Keys ?? $jsonObj.inputs.Keys
+
+        foreach ($inputKey in $inputKeys) {
+            if (!($inputKey -ceq $inputKey.ToLower())) {
+                Write-Host “Input variable '$inputKey' contains uppercase characters”
+
+                $isValid = $false
+            }
+        }
+    }
+
+    return $isValid
+}
