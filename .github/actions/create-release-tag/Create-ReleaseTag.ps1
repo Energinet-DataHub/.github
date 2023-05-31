796f07f10776bee5b29a676fc0fea3550469fcf3
rf-halfspace <rf@halfspace.io>
2023-05-31 10:24:32 +0200

    Output GH Token

--- .github/actions/create-release-tag/Create-ReleaseTag.ps1
+++ .github/actions/create-release-tag/Create-ReleaseTag.ps1
@@ -63,6 +63,7 @@ function Create-ReleaseTag {
 
     $version = "$MajorVersion.$MinorVersion.$PatchVersion"
 
+    Write-Host "GH_TOKEN:" $env:GH_TOKEN
     if ([string]::IsNullOrEmpty($env:GH_TOKEN)) {
         throw "Error: GH_TOKEN environment variable is not set, see https://cli.github.com/manual/gh_auth_login for details"
     }
