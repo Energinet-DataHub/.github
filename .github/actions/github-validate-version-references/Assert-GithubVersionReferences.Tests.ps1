be82e1293a1f964a98258d9b6084c887c3ce2083
rf-halfspace <rf@halfspace.io>
2023-06-26 15:14:26 +0200

    Review changes

--- .github/actions/github-validate-version-references/Assert-GithubVersionReferences.Tests.ps1
+++ .github/actions/github-validate-version-references/Assert-GithubVersionReferences.Tests.ps1
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-Describe "Assert-GithubVersionReferences" {
+Describe "Get-LatestMajorVersion" {
 
     BeforeAll {
         . $PSScriptRoot/Assert-GithubVersionReferences.ps1
@@ -40,17 +40,40 @@ Describe "Assert-GithubVersionReferences" {
         }
     }
 
-    Context "Get-LatestMajorVersion" {
-        BeforeEach {
-
-        }
-
-        It "Returns expected version" {
+    Context "Given list of releases returned by API" {
+        It "It should return the highest major version" {
             Get-LatestMajorVersion -Repository "mock" | Should -Be 50
         }
     }
+}
 
-    Context "Assert-GithubVersionReferences checks github action references" {
+Describe "Assert-GithubVersionReferences" {
+    BeforeAll {
+        . $PSScriptRoot/Assert-GithubVersionReferences.ps1
+
+        Mock Invoke-GetGithubReleases {
+            return @"
+            "title","type","tagname","published"
+            "50.2.0","Latest","50.2.0","2023-05-10T12:57:40Z"
+            "v50","Draft","v50","2023-05-10T12:56:36Z"
+            "50.1.0","","50.1.0","2023-05-08T08:31:58Z"
+            "50.0.2","","50.0.2","2023-05-01T07:05:42Z"
+            "50.0.1","","50.0.1","2023-04-28T11:49:23Z"
+            "50.0.0","","50.0.0","2023-04-27T07:39:41Z"
+            "49.2.2","","49.2.2","2023-02-16T10:25:41Z"
+            "49.2.1","","49.2.1","2023-02-16T09:54:53Z"
+            "49.2.0","","49.2.0","2023-01-26T08:37:05Z"
+            "49.1.2","","49.1.2","2023-01-23T09:49:15Z"
+            "49.1.1","","49.1.1","2023-01-20T10:09:09Z"
+            "49.1.0","","49.1.0","2023-01-19T10:37:38Z"
+            "49.0.0","","49.0.0","2023-01-18T07:38:37Z"
+"@ | ConvertFrom-Csv
+        }
+        if ([string]::IsNullOrEmpty($env:GH_TOKEN)) {
+            $env:GH_TOKEN = $(gh auth token)
+        }
+    }
+    Context "When testing text containing a usage pattern for .github repo" {
         BeforeEach {
             Mock Get-ChildItem {
                 return @{"FullName" = "mock" }
@@ -63,15 +86,17 @@ Describe "Assert-GithubVersionReferences" {
 "@ -f "v49"
             }
         }
-        It "Thows when expected" {
-            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 2 } | Should -Not -Throw
-            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 49 } | Should -Not -Throw
+        It "Throws an exception when version is not supported" {
             { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 1 } | Should -Throw
             { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 0 } | Should -Throw
         }
+        It "Doesn't throw when version is supported" {
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 2 } | Should -Not -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 49 } | Should -Not -Throw
+        }
     }
 
-    Context "Assert-GithubVersionReferences checks terraform module references" {
+    Context "When testing text containing a usage pattern for geh-terraform-modules repo" {
         BeforeEach {
             Mock Get-ChildItem {
                 return @("mock")
@@ -85,11 +110,13 @@ Describe "Assert-GithubVersionReferences" {
 "@ -f "v49"
             }
         }
-        It "Thows when expected" {
-            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 2 } | Should -Not -Throw
-            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 49 } | Should -Not -Throw
+        It "Throws an exception when version is not supported" {
             { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 1 } | Should -Throw
             { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 0 } | Should -Throw
+        }
+        It "Doesn't throw when version is supported" {
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 2 } | Should -Not -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 49 } | Should -Not -Throw
         }
     }
 }
