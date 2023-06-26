c57ce32954752a8db6ed0e861d6213368f38b4d0
rf-halfspace <rf@halfspace.io>
2023-06-26 13:57:45 +0200

    Revert to "Changed to use checkout instead of gh api"

--- .github/actions/github-validate-version-references/Assert-GithubVersionReferences.Tests.ps1
+++ .github/actions/github-validate-version-references/Assert-GithubVersionReferences.Tests.ps1
@@ -16,9 +16,38 @@ Describe "Assert-GithubVersionReferences" {
 
     BeforeAll {
         . $PSScriptRoot/Assert-GithubVersionReferences.ps1
+
+        Mock Get-GithubReleases {
+            return @"
+            "title","type","tagname","published"
+            "50.2.0","Latest","50.2.0","2023-05-10T12:57:40Z"
+            "v50","Draft","v50","2023-05-10T12:56:36Z"
+            "50.1.0","","50.1.0","2023-05-08T08:31:58Z"
+            "50.0.2","","50.0.2","2023-05-01T07:05:42Z"
+            "50.0.1","","50.0.1","2023-04-28T11:49:23Z"
+            "50.0.0","","50.0.0","2023-04-27T07:39:41Z"
+            "49.2.2","","49.2.2","2023-02-16T10:25:41Z"
+            "49.2.1","","49.2.1","2023-02-16T09:54:53Z"
+            "49.2.0","","49.2.0","2023-01-26T08:37:05Z"
+            "49.1.2","","49.1.2","2023-01-23T09:49:15Z"
+            "49.1.1","","49.1.1","2023-01-20T10:09:09Z"
+            "49.1.0","","49.1.0","2023-01-19T10:37:38Z"
+            "49.0.0","","49.0.0","2023-01-18T07:38:37Z"
+"@ | ConvertFrom-Csv
+        }
     }
 
-    Context "When a pull-request contains deprecated references" {
+    Context "Get-LatestMajorVersion" {
+        BeforeEach {
+
+        }
+
+        It "Returns expected version" {
+            Get-LatestMajorVersion -Repository "mock" | Should -Be 50
+        }
+    }
+
+    Context "Assert-GithubVersionReferences checks github action references" {
         BeforeEach {
             Mock Get-ChildItem {
                 return @{"FullName" = "mock" }
@@ -31,34 +60,33 @@ Describe "Assert-GithubVersionReferences" {
 "@ -f "v49"
             }
         }
-        It "Throws an exception when major version is just too high" {
-            Mock Get-LatestMajorVersion {
-                return [int]51
-            }
-
-            { Assert-DotGithubVersionReferences -Path "mock" -RepositoryPath "mock" } | Should -Throw
+        It "Thows when expected" {
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 2 } | Should -Not -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 49 } | Should -Not -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 1 } | Should -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 0 } | Should -Throw
         }
-        It "Throws an exception when major version is way too high" {
-            Mock Get-LatestMajorVersion {
-                return [int]100
-            }
+    }
 
-            { Assert-DotGithubVersionReferences -Path "mock" -RepositoryPath "mock" } | Should -Throw
+    Context "Assert-GithubVersionReferences checks terraform module references" {
+        BeforeEach {
+            Mock Get-ChildItem {
+                return @("mock")
+            }
+            Mock Get-Content {
+                # Note: In order not to have this test-file introduce false possitives string formatting is used to avoid the regex pattern.
+                return @"
+                mock
+                    source = `"https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/subpath/module?ref={0}`"
+                mock
+"@ -f "v49"
+            }
         }
-
-        It "Doesn't throws when version is current latest" {
-            Mock Get-LatestMajorVersion {
-                return [int]49
-            }
-
-            { Assert-DotGithubVersionReferences -Path "mock" -RepositoryPath "mock" } | Should -Not -Throw
-        }
-
-        It "Doesn't throws when version is maintenance mode version" {
-            Mock Get-LatestMajorVersion {
-                return [int]48
-            }
-            { Assert-DotGithubVersionReferences -Path "mock" -RepositoryPath "mock" } | Should -Not -Throw
+        It "Thows when expected" {
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 2 } | Should -Not -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 49 } | Should -Not -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 1 } | Should -Throw
+            { Assert-GithubVersionReferences -Path "Path" -MajorVersionsToKeep 0 } | Should -Throw
         }
         It "Doesn't throws when version is way lower than referenced" {
             Mock Get-LatestMajorVersion {
