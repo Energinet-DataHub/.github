051f2d898e8297ed9067edbf989d2bb105ad5ad8
rf-halfspace <rf@halfspace.io>
2024-05-24 09:57:24 +0200

    prepare for pipeline test

---
+++ .github/actions/automatic-release/Create-GitHubRelease.ps1
@@ -0,0 +1,136 @@
+<#
+    .SYNOPSIS
+    Github Action script creating automated releases
+
+    .DESCRIPTION
+    The functionality is executed as an action on a github runner and creates automated releases for pull requests.
+#>
+
+param(
+    [Parameter(Mandatory)]
+    [string]$GithubContext
+)
+
+if ([string]::IsNullOrEmpty($env:GH_TOKEN)) {
+    throw "Error: GH_TOKEN environment variable is not set, see https://cli.github.com/manual/gh_auth_login for details"
+}
+$GithubRepository = $GithubContext | ConvertFrom-Json | Select-Object -ExpandProperty repository | Select-Object -ExpandProperty full_name
+
+<#
+    .SYNOPSIS
+    Class representing a Github Release output using  github CLI (gh)
+
+    .DESCRIPTION
+    Simple class modelling the output from a gh release list command into typed.
+#>
+class GithubRelease {
+    [string]$name
+    [string]$tagName
+    [string]$publishedAt
+    [string]$isPrerelease
+    [string]$isLatest
+    [string]$isDraft
+}
+
+<#
+    .SYNOPSIS
+    Uses github CLI (gh) to retrieves a list of releases
+
+    .DESCRIPTION
+    Simple function wrapping a call with gh to retrieve the latest releases from github.
+#>
+function Publish-GitHubRelease {
+    param (
+        [Parameter(Mandatory)]
+        [string]$TagName,
+        [Parameter(Mandatory)]
+        [string]$Title,
+        [Parameter(Mandatory)]
+        [string[]]$Files,
+        [string]$PreRelease = $false,
+        [string]$Draft = $false
+    )
+
+    # Get Previous Release
+    [GithubRelease]$release = Invoke-GithubReleaseList -TagName $TagName
+
+    # Delete Previous Release
+    $release | Invoke-GithubReleaseDelete
+
+    # Create release
+    Invoke-GithubReleaseCreate -TagName $TagName -Title $Title -PreRelease $PreRelease -Draft $Draft -Files $Files
+}
+
+<#
+    .SYNOPSIS
+    Uses github CLI (gh) to retrieves a list of releases
+
+    .DESCRIPTION
+    Simple function wrapping a call with gh to retrieve the latest releases from github.
+#>
+function Invoke-GithubReleaseList {
+    param (
+        [string]$TagName
+    )
+    gh release list -L 10000 -R $GithubRepository --json name, tagName, publishedAt, isPrerelease, isLatest, isDraft `
+    | ConvertFrom-Json
+    | Where-Object { $_.name -eq $TagName }
+}
+
+<#
+    .SYNOPSIS
+    Uses github CLI (gh) to delete a release
+
+    .DESCRIPTION
+    Simple function wrapping a call with gh to delete a release from github.
+#>
+function Invoke-GithubReleaseDelete {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        [GithubRelease]$release
+    )
+
+    if ($null -eq $release) {
+        Write-Warning "No release to delete."
+        return $release
+    }
+
+    Write-Host "Deleting $($release.Name)"
+    gh release delete $release.Name -y --cleanup-tag -R $GitHubRepository
+}
+
+<#
+    .SYNOPSIS
+    Uses github CLI (gh) to delete a release
+
+    .DESCRIPTION
+    Simple function wrapping a call with gh to delete a release from github.
+#>
+function Invoke-GithubReleaseCreate {
+    param(
+        [string]$TagName,
+        [string]$Title,
+        [string[]]$Files,
+        [string]$PreRelease = $false,
+        [string]$Draft = $false
+    )
+
+    $cmdbuilder = @(
+        "gh release create"
+        $TagName,
+        "-R $GithubRepository"
+        "--generate-notes"
+    )
+    if ($PreRelease) {
+        $cmdbuilder += "--prerelease"
+    }
+    if ($Draft) {
+        $cmdbuilder += "--draft"
+    }
+    $cmdbuilder += $Files
+
+    $cmd = $cmdbuilder -join " "
+
+    Write-Host $cmd
+}
---
+++ .github/actions/automatic-release/Create-GithubRelease.Tests.ps1
@@ -0,0 +1,65 @@
+BeforeAll {
+    $env:GH_TOKEN = "test"
+    $context = Get-Content $PSScriptRoot/blabla.json | Out-String
+    . $PSScriptRoot/Create-GithubRelease.ps1 -GithubContext $context
+}
+
+Describe "Create-AutomaticGitHubRelease" -ForEach @(
+    @{ Releases = @("v1"); TagName = "v1"; Expected = "v1" },
+    @{ Releases = @("v1", "v2"); TagName = "v1"; Expected = "v1" },
+    @{ Releases = @("not_v1"); TagName = "v1"; Expected = $null }
+) {
+
+    BeforeEach {
+        ## Mock gh release list
+        Mock -CommandName gh `
+            -ParameterFilter { $args[0] -eq "release" -and $args[1] -eq "list" } `
+            -MockWith {
+            Write-Warning "Mocked gh release list ..."
+            $Releases | ForEach-Object { [GithubRelease]@{ Name = $_ } } | ConvertTo-Json -AsArray
+        }
+
+        ## Mock gh release delete
+        Mock -CommandName gh `
+            -ParameterFilter { $args[0] -eq 'release' -and $args[1] -eq "delete" } `
+            -MockWith {
+            Write-Warning "Mocked gh release delete ..."
+            $true
+        }
+
+        ## Mock gh release create
+        Mock -CommandName gh `
+            -ParameterFilter { $args[0] -eq 'release' -and $args[1] -eq "create" } `
+            -MockWith {
+            Write-Warning "Mocked gh release create ..."
+            $true
+        }
+    }
+    It "Returns <expected> when searching for <TagName>" {
+        $previousRelease = Invoke-GithubReleaseList -TagName $TagName
+        $previousRelease.Name | Should -Be $expected
+    }
+
+    It "Deletes <expected> when provided <TagName>" {
+        $previousRelease = Invoke-GithubReleaseList -TagName $TagName
+        $previousRelease | Invoke-GithubReleaseDelete
+        if ($Expected) {
+            Should -Invoke -CommandName "gh" -Exactly -Times 2
+        }
+        else {
+            Should -Invoke -CommandName "gh" -Exactly -Times 1
+        }
+    }
+
+    It "Creates <expected> when provided <tagname>" {
+        $Files = "$PSScriptRoot\sauron_infrastructure_1496.zip"
+        Invoke-GithubReleaseCreate -TagName $TagName -Title "title" -PreRelease $true -Draft $false -Files $Files
+        if ($Expected) {
+            Should -Invoke -CommandName "gh" -Exactly -Times 1
+        }
+        else {
+            Should -Invoke -CommandName "gh" -Exactly -Times 0
+        }
+    }
+}
+
