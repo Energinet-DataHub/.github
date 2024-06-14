543ceb3c6b7f129e6bfb205a016f772260db92ca
Kristian Bank Erbou <contact@kbeconsult.dk>
2024-06-14 17:09:53 +0200

    ...

--- .github/actions/azure-login/New-AzureLogin.ps1
+++ .github/actions/azure-login/New-AzureLogin.ps1
@@ -8,28 +8,34 @@ param (
     $SubscriptionId
 )
 
-function Set-GhEnvVar($Name, $Value) {
-
-}
-
-$azBasePath = $isLinux ? '/usr/share' : 'C:\Modules'
-Write-Host 'azBasePath: ', $azBasePath
-$azModule = Get-ChildItem -Directory "$azBasePath/az*" -ErrorAction Stop | Select-Object -Last 1
-$newPSModulePath = $azModule.FullName, $env:PSModulePath -join [io.path]::PathSeparator
-$env:PSModulePath = $newPSModulePath
-
-# #Export to additional steps in the job
-"$PSModulePath=$env:PSModulePath" >> $env:GITHUB_ENV
-
-$oidcTokenParams = @{
-    Uri            = $env:ACTIONS_ID_TOKEN_REQUEST_URL
-    Body           = @{
-        audience = 'api://AzureADTokenExchange'
+function Get-GitHubOIDCToken {
+    $oidcTokenParams = @{
+        Uri            = $env:ACTIONS_ID_TOKEN_REQUEST_URL
+        Body           = @{
+            audience = 'api://AzureADTokenExchange'
+        }
+        Authentication = 'Bearer'
+        Token          = $env:ACTIONS_ID_TOKEN_REQUEST_TOKEN | ConvertTo-SecureString -AsPlainText
     }
-    Authentication = 'Bearer'
-    Token          = $env:ACTIONS_ID_TOKEN_REQUEST_TOKEN | ConvertTo-SecureString -AsPlainText
+  (Invoke-RestMethod @oidcTokenParams).value
 }
-$token = (Invoke-RestMethod @oidcTokenParams).value
+
+function Set-GhEnvVar($Name, $Value) { "$Name=$Value" >> $env:GITHUB_ENV }
+
+function Add-AzModuleToPath {
+    if ($isMacOS) { throw 'Not supported on MacOS' }
+    $azBasePath = $isLinux ? '/usr/share' : 'C:\Modules'
+    $azModule = Get-ChildItem -Directory "$azBasePath/az*" -ErrorAction Stop | Select-Object -Last 1
+    $newPSModulePath = $azModule.FullName, $env:PSModulePath -join [io.path]::PathSeparator
+    $env:PSModulePath = $newPSModulePath
+}
+
+
+#region Main
+$token = Get-GitHubOIDCToken
+Add-AzModuleToPath
+#Export to additional steps in the job
+Set-GhEnvVar 'PSModulePath' $env:PSModulePath
 
 Clear-AzContext -Force #This is only necessary on self-hosted runners
 $connectAzAccountParams = @{
@@ -46,7 +52,7 @@ $context = Connect-AzAccount @connectAzAccountParams
 if (-not $context) {
     throw 'Connect-AzAccount ran but no context was returned. This is probably a bug.'
 }
-Write-Host "Connected to $($context.Context.Account)"
+"Connected to $($context.Context.Account)"
 
 Write-Host 'Logging in to Azure CLI...'
 az login --service-principal --tenant $TenantId --username $ClientId --federated-token $token | Out-Null
